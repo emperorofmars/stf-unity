ac0668ff959244c37a332a30d58403836bfa6cfa
emperorofmars <contact@emperorofmars.org>
2023-11-29 03:22:26 +0100

    ASTFApplicationConverter

--- STF/Runtime/ApplicationConversion/Converters/STFUnityConverter.cs
+++ STF/Runtime/ApplicationConversion/Converters/STFUnityConverter.cs
@@ -8,64 +8,27 @@ using UnityEngine.Animations;
 
 namespace STF.ApplicationConversion
 {
-	public class STFUnityConverter : ISTFApplicationConverter
+	public class STFUnityConverter : ASTFApplicationConverter
 	{
-		public string TargetName => "unity3d";
+		public const string _TARGET_NAME = "unity3d";
+		public override string TargetName => _TARGET_NAME;
 
-		private List<Type> WhitelistedComponents => new List<Type> {
-			typeof(Transform), typeof(Animator), typeof(RotationConstraint), typeof(SkinnedMeshRenderer)
-		};
 
-		private Dictionary<Type, ISTFNodeComponentApplicationConverter> Converters => new Dictionary<Type, ISTFNodeComponentApplicationConverter>() {
+		private Dictionary<Type, ISTFNodeComponentApplicationConverter> _Converters = new Dictionary<Type, ISTFNodeComponentApplicationConverter>() {
 			{typeof(STFTwistConstraint), new STFTwistConstraintConverter()},
 		};
+		public override Dictionary<Type, ISTFNodeComponentApplicationConverter> Converters => _Converters;
 
-		public bool CanConvert(STFAsset Asset)
+		private List<Type> _WhitelistedComponents = new List<Type> {
+			typeof(Transform), typeof(Animator), typeof(RotationConstraint), typeof(SkinnedMeshRenderer)
+		};
+		public override List<Type> WhitelistedComponents => _WhitelistedComponents;
+
+		public override List<string> Targets => new List<string> {_TARGET_NAME};
+
+		public override bool CanConvert(STFAsset Asset)
 		{
 			return Asset.assetInfo?.assetType == STFAssetImporter._TYPE;
-		}
-
-		public GameObject Convert(ISTFApplicationConvertStorageContext StorageContext, STFAsset Asset)
-		{
-			GameObject ret = null;
-			STFApplicationConvertState state = null;
-			try
-			{
-				ret = UnityEngine.Object.Instantiate(Asset.gameObject);
-				ret.name = Asset.gameObject.name;
-
-				state = new STFApplicationConvertState(StorageContext, ret, TargetName, new List<string>{TargetName}, Converters.Keys.ToList());
-
-				foreach(var component in ret.GetComponentsInChildren<Component>())
-				{
-					if(Converters.ContainsKey(component.GetType()))
-					{
-						Converters[component.GetType()].Convert(state, component);
-					}
-				}
-				state.RunTasks();
-
-				foreach(var component in ret.GetComponentsInChildren<Component>())
-				{
-					if(!WhitelistedComponents.Contains(component.GetType()))
-					{
-						#if UNITY_EDITOR
-							UnityEngine.Object.DestroyImmediate(component);
-						#else
-							UnityEngine.Object.Destroy(component);
-						#endif
-					}
-				}
-				return ret;
-			}
-			catch(Exception e)
-			{
-				throw new Exception("Error during STF conversion: ", e);
-			}
-			finally
-			{
-				state?.DeleteTrash();
-			}
 		}
 	}
 }
--- STF/Runtime/ApplicationConversion/Interfaces/ISTFApplicationConverter.cs
+++ STF/Runtime/ApplicationConversion/Interfaces/ISTFApplicationConverter.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using STF.IdComponents;
 using UnityEngine;
@@ -7,8 +9,62 @@ namespace STF.ApplicationConversion
 {
 	public interface ISTFApplicationConverter
 	{
-		string TargetName {get;}
 		bool CanConvert(STFAsset Asset);
 		GameObject Convert(ISTFApplicationConvertStorageContext StorageContext, STFAsset Asset);
+	}
+	
+
+	public abstract class ASTFApplicationConverter : ISTFApplicationConverter
+	{
+		//protected abstract Dictionary<Type, ISTFSecondStageResourceProcessor> ResourceProcessors {get;}
+		public abstract Dictionary<Type, ISTFNodeComponentApplicationConverter> Converters {get;}
+		public abstract List<Type> WhitelistedComponents {get;}
+		public abstract string TargetName {get;}
+		public abstract List<string> Targets {get;}
+
+		public abstract bool CanConvert(STFAsset Asset);
+
+		public GameObject Convert(ISTFApplicationConvertStorageContext StorageContext, STFAsset Asset)
+		{
+			GameObject ret = null;
+			STFApplicationConvertState state = null;
+			try
+			{
+				ret = UnityEngine.Object.Instantiate(Asset.gameObject);
+				ret.name = Asset.gameObject.name + "_" + TargetName;
+
+				state = new STFApplicationConvertState(StorageContext, ret, TargetName, Targets, Converters.Keys.ToList());
+
+				foreach(var component in ret.GetComponentsInChildren<Component>())
+				{
+					if(Converters.ContainsKey(component.GetType()))
+					{
+						Converters[component.GetType()].Convert(state, component);
+					}
+				}
+				state.RunTasks();
+
+				foreach(var component in ret.GetComponentsInChildren<Component>())
+				{
+					if(!WhitelistedComponents.Contains(component.GetType()))
+					{
+						#if UNITY_EDITOR
+							UnityEngine.Object.DestroyImmediate(component);
+						#else
+							UnityEngine.Object.Destroy(component);
+						#endif
+					}
+				}
+				return ret;
+			}
+			catch(Exception e)
+			{
+				throw new Exception("Error during STF conversion: ", e);
+			}
+			finally
+			{
+				state?.DeleteTrash();
+			}
+		}
 	}
 }
